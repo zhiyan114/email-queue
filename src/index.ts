2a50c0f9e0e35c2f4819005cbb60d40b4fd0b462
zhiyan114 <zhiyan114@furries.dev>
2025-10-09 00:55:33 -0400

    Finish Software (need docker and SDK dev)

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("HELLO WORLD!");
+import { Client } from 'pg';
+import { QueueManager } from './queueManager';
+import { WebSrvManager } from './webserverHandle';
+import { captureException } from '@sentry/node-core';
+
+const pgClient = new Client(process.env["PGSQL_CONN"]);
+const queueMGR = new QueueManager(pgClient);
+const webSrvMGR = new WebSrvManager(pgClient, queueMGR);
+
+
+pgClient.connect().then(()=>{
+  if(! process.env["SMTP_CONN"] || !process.env["AMQP_CONN"])
+    throw Error("Queue Manager Cannot Be Initialize: Missing SMTP_CONN or AMQP_CONN env variable");
+
+  queueMGR.setup(process.env["SMTP_CONN"], process.env["AMQP_CONN"]);
+  webSrvMGR.setup(process.env["PORT"] ? parseInt(process.env["PORT"]) : 80);
+}).catch(ex=>captureException(ex));
